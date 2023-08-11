e7e1a7dd27a988c993e78f6ece69ef0f5e5cab4d
Lucas Martinez <lucas.oromi@gmail.com>
2023-08-10 23:37:30 -0600

    some on dynamic objects

--- Inter/XState/Class1.cs
+++ Inter/XState/Class1.cs
@@ -1,7 +1,52 @@
-﻿namespace XState
+﻿using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace XState
 {
     public class Class1
     {
+        System.Text.Json.JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        public string Serialize(object obj)
+        {
+            return JsonSerializer.Serialize(obj, options);
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            var root = JsonNode.Parse(json);
+            if (root!.GetType() == typeof (JsonArray))
+            {
+                var array = root.AsArray();
+                foreach (var item in array)
+                {
+                    if (item!.GetType() == typeof (JsonObject))
+                    {
+                        var obj = item.AsObject();
+                        obj.Add("test", "test");
+                    }
+                }
+            }
+            else if (root.GetType() == typeof (JsonObject))
+            {
+                var obj = root.AsObject();
+                obj.Add("test", "test");
+            }
+            else
+            {
+                root!.AsObject().Add("test", "test");
+            }
+            root!.AsObject().Add("test", "test");
+            JsonDocument doc = JsonDocument.Parse(json);
+            if (doc.RootElement.ValueKind == JsonValueKind.Array)
+            {
+            }
+            return JsonSerializer.Deserialize<T>(json, options);
+        }
 
     }
 }
